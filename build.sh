d357eb9a83d15b1502916d5b99f16ce771b7a07b
Amith Koujalgi <amith.koujalgi@razorthink.com>
2016-04-04 21:32:17 +0530

    init

--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@ cd ~/
 git clone https://github.com/koujalgi-amith/websequence-diagrams-maven-plugin.git
 cd websequence-diagrams-maven-plugin
 mvn clean install
+
+file=~/.m2/repository/com/websequencediagrams/websequencediagrams-plugin/0.0.1/websequencediagrams-plugin-0.0.1.jar
+if [ -f "$file" ]
+then
+	echo "Plugin installed."
+else
+	echo "Plugin couldn't be installed."
+fi
